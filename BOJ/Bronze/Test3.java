6209292be775ebd688ce547d418e15e4bb1680c9
jindream6128 <jindream6128@naver.com>
2022-12-31 02:32:45 +0900

    git 초기설정2

--- BOJ/Bronze/Test3.java
+++ BOJ/Bronze/Test3.java
@@ -1,6 +1,16 @@
 public class Test3 {
     public static void main(String[] args) {
-        System.out.println("test");
+        double nominal = 1.0;
+        double sum = 0.0;
+
+        for (int i = 0; i < 10; i++) {
+            sum += 0.1;
+        }
+        if (nominal == sum) {
+            System.out.println("Same" + sum);
+        } else {
+            System.out.println("Different" + sum);
+        }
     }
 
 }
