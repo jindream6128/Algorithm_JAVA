002b2a913679142b2aeff8200d8f1b63b2c3321e
jindream6128 <jindream6128@naver.com>
2023-01-15 02:17:06 +0900

    algorithm 230114

--- BOJ/Bronze/boj_1816.java
+++ BOJ/Bronze/boj_1816.java
@@ -5,37 +5,40 @@ public class boj_1816 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
-        int[] sosu = new int[1000001];
-
-        for (int i = 1; i < 1000001; i++) {
-            //소수 이면 1 소수가 아니면 2
-            sosu[i] = 1;
-        }
-
-        //소수가 아닌거 골라야함
-        for (int i = 1; i < 1000001; i++) {
-            if (sosu[i] == 1) {//소수일때
-                for (int j = i * i; j < 1000001; j++) {
-                    sosu[j] = 0; //소수의 제곱은 소수가 아니다.
+        for(int i = 0; i< n; i++){
+            //s 입력받기
+            long s = Long.parseLong(br.readLine());
+            // s만큼의 arr받기
+            int[] s_arr = new int[1000001];
+            String result = "YES";
+            for(int j =2; j <1000001; j++){
+                if(s%j==0){
+                    result = "NO";
                 }
-            }
-        }
-
-        long s = Long.parseLong(br.readLine());
-        for (int i = 0; i < n; i++) {
-            String result = "YES"; //소수이다
-
-            for (int j = 0; j < 1000001; j++) {
-                if (sosu[j] == 1) { //소수일때
-                    if (s % sosu[j] == 0) {
-                        result = "NO";
-                    }
-                }
-
             }
             bw.write(result);
+            bw.newLine();
+
+
+
+            // 0이면 약수가 아님 1이면 약수
+//            for(int j =2; j<s+1;j++){
+//                if(s%j==0) {
+//                    s_arr[j] = 1;
+//                }
+//            }
+//            String result = "NO"; //소수가 아님
+//            for(int j =2; j<s+1;j++){
+//                if(s_arr[j]==1){
+//                    if(j>1000000){
+//                        result = "YES";
+//                    }
+//                }
+//            }
+//            bw.write(result);
 
         }
+
 
         br.close();
         bw.flush();
