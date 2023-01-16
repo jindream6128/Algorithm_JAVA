d944eb4a6edc81fc9111b3a9d98141140a211c0d
jindream6128 <jindream6128@naver.com>
2023-01-16 18:44:04 +0900

    algorithm 230116

---
+++ BOJ/Bronze/boj_2742.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class boj_2742 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        for(int i = n; i>0;i--){
+            bw.write(String.valueOf(i));
+            bw.newLine();
+        }
+        br.close();
+        bw.close();
+    }
+}
