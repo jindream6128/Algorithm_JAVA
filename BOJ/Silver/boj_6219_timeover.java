cc422c015389a90c03a735e2390f0660f6d94a63
jindream6128 <jindream6128@naver.com>
2023-01-20 03:13:53 +0900

    algorithm 230119

---
+++ BOJ/Silver/boj_6219_timeover.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class boj_6219_timeover {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader ( new InputStreamReader(System.in));
+        // A랑 B사이의 범위의 소수를 모두 구한다
+        // 그 소수들 중에서 D를 포함하는지 확인하기
+        // Contains 메서드를 통해서 D가 포함되는지 확인하기
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        String s = st.nextToken();
+        //소수 찾기 a부터 b까지 -> 리스트에 저장
+        ArrayList<String> arr = new ArrayList<>();
+
+        for(int i = a ; i<b+1; i++){
+            int cnt = 0;
+            for(int j =1; j*j<=i; j++){
+                if(i%j == 0){
+                    cnt++;
+                }
+            }
+            if(cnt == 1){
+                arr.add(String.valueOf(i));
+            }
+        }
+        //소수만 있는 arr중에서 D가 포함이 되면 True 그러면 ans ++
+        int ans = 0;
+        for(int i = 0 ; i<arr.size();i++){
+            if(arr.get(i).contains(s)){
+                ans++;
+            }
+        }
+
+        System.out.println(ans);
+
+        br.close();
+
+    }
+}
