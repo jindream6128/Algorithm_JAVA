3bfb6c18efb08e531d3cce6055e0b8bc3b3d32d5
jindream6128 <jindream6128@naver.com>
2023-01-23 03:17:32 +0900

    algorithm 230122

--- BOJ/Silver/boj_2725.java
+++ BOJ/Silver/boj_2725.java
@@ -8,13 +8,36 @@ public class boj_2725 {
     // 서로다른 기울기 -> 분모, 분자의 최대공약수가 1인 수를 찾으면 된다.
 
     //최대공약수 구하기 gcd
-
+    static int gcd(int a, int b){
+        int tmp;
+        //항상 앞에가 더 크도록
+        if(a<b){tmp = b; b=a;a=tmp;}
+        while(a%b>0){
+            //a%b!=0
+            tmp =b;
+            b = a%b;
+            a = tmp;
+        }
+        return b;
+    }
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int tc = Integer.parseInt(br.readLine());
         for(int i = 0; i <tc;i++){
             int n = Integer.parseInt(br.readLine());
+            int cnt = 0; // 기울기가 1인거 한개
+            for(int a = 1; a<n+1;a++){
+                //b는 a까지만 돌고 나중에 *2해주기
+                for(int b = 1;b<a;b++){
+                    //만약 최대공약수가 1이면 기울기가 다른거이므로 cnt +1
+                    if(gcd(a,b)==1){
+                        cnt++;
+                    }
+                }
+            }
+            //기울기가 1인거랑, x,y축 에 올라가있는거 각 1개씩 총2개
+            System.out.println(cnt*2+3);
         }
 
         br.close();
