e31f778150e7ea41fa7cef16fad73c4f3e99f2ad
jindream6128 <jindream6128@naver.com>
2023-02-15 14:23:58 +0900

    algorithm 230215 시뮬레이션 한수

--- BOJ/Silver/boj_4673/boj_4673.java
+++ BOJ/Silver/boj_4673/boj_4673.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 public class boj_4673 {
 
+    //기본 불리언 값음 false
     static boolean[] num = new boolean[10_001];
 
     //dn구하기
@@ -11,12 +12,12 @@ public class boj_4673 {
         //각 자리를 구하기 최대 5자리이니까 5개 배열 구하고 거기에 넣기
         int[] arr = new int[5];
         int i =0;
+        int ans = n;
         while (n > 0) {
             arr[i] = n%10;
             n = n/10;
             i++;
         }
-        int ans = n;
         for(int j = 0; j<5;j++){
             ans += arr[j];
         }
@@ -25,15 +26,17 @@ public class boj_4673 {
 
     public static void main(String[] args){
         StringBuilder sb = new StringBuilder();
-
-        for(int i = 1; i<=10_000;i++){
-            num[dn(i)] = true;
+        for(int i =1; i<=10_000; i++){
+            if(dn(i)<=10_000){
+                num[dn(i)] = true;
+            }
         }
-        for(int i = 1;i<=10_000;i++){
-            if(num[i] == false){
+        for(int  i =1; i<=10_000;i++){
+            if(!num[i]){ //num[i] == false를 해버리면 false 값들인 값들이 true결과를 반환하면서 내가 생각했던거와 반대로 출력된다.
                 sb.append(i).append("\n");
             }
         }
+
         System.out.println(sb);
     }
 }
---
+++ BOJ/basic/exampletest.java
@@ -0,0 +1,9 @@
+public class exampletest {
+    public static void main(String[] args){
+
+        boolean test = false;
+        if(test == false) {
+            System.out.println("정답");
+        }
+    }
+}
