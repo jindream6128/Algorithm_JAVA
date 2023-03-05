4f5c55767de9889a1603c33bf9b56cf833144358
jindream6128 <jindream6128@naver.com>
2023-03-05 17:33:26 +0900

    algorithm 230305 (나무 자르기) 이분 탐색 Silver2 *****

---
+++ BOJ/Silver/boj_2805/boj_2805_2.java
@@ -0,0 +1,49 @@
+package boj_2805;
+import java.io.*;
+import java.util.*;
+
+public class boj_2805_2 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        //나무의 수
+        int n = Integer.parseInt(st.nextToken());
+        //집에 가지고 가야하는 나무길이
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+
+        //두번째 줄까지 각 나무길이 까지 입력
+        for(int i =0 ;i < n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        int min = 0;
+        int max = arr[n-1];
+        int ans =0;
+
+        while(min<=max){
+            long tmpSum = 0;
+            int mid = (min + max) /2;
+            for(int i = 0; i<n; i++){
+                if((arr[i] - mid) >0){
+                    tmpSum += (arr[i] - mid);
+                }
+            }
+
+            if(tmpSum >= m){
+                min = mid +1;
+                ans = min-1;
+            }else{
+                max = mid -1;
+            }
+        }
+
+        System.out.println(ans);
+
+        br.close();
+    }
+}
