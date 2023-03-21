e4026b7a1c8b060e4942dd5b107e5d962814b41a
jindream6128 <jindream6128@naver.com>
2023-03-21 21:47:10 +0900

    algorithm 230321 (1, 2, 3 더하기2) 백트래킹, DFS silver2 *****

---
+++ BOJ/Gold/boj_2661/boj_2661.java
@@ -0,0 +1,26 @@
+package boj_2661;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj_2661 {
+    static int n;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+
+        br.close();
+    }
+
+    static void recur(String str){
+        if(str.length()==n){
+            sb.append(str);
+            return;
+        }
+
+        for(int i =1;)
+    }
+}
