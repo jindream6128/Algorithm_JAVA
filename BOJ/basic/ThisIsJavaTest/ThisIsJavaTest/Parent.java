248ebad289d74c2979a9092219b734de11840d12
jindream6128 <jindream6128@naver.com>
2023-03-23 20:09:02 +0900

    days4

--- BOJ/basic/ThisIsJavaTest/ThisIsJavaTest/Parent.java
+++ BOJ/basic/ThisIsJavaTest/ThisIsJavaTest/Parent.java
@@ -1,15 +1,31 @@
 package ThisIsJavaTest;
 
+import java.util.*;
+
 public class Parent {
-    public String nation;
+public static void main(String[] args){
+    ArrayList<String> duplicate_data = new ArrayList<String>();
+    duplicate_data.add("hihihi");
+    duplicate_data.add("hihihi");
+    duplicate_data.add("hihihi");
+    duplicate_data.add("hello");
+    duplicate_data.add("hello");
+    duplicate_data.add("Java");
+    duplicate_data.add("Java");
+    duplicate_data.add("Yang");
+    duplicate_data.add("Yang's blog");
+    duplicate_data.add("Yang's blog");
 
-    public Parent() {
-        this("대한민국");
-        System.out.println("Paretn() call");
+    HashMap<String, Integer> duplicate_count = new HashMap<String, Integer>();
+
+    for(int i = 0 ; i < duplicate_data.size() ; i++){ // ArrayList 만큼 반복
+        if (duplicate_count.containsKey(duplicate_data.get(i))) { // HashMap 내부에 이미 key 값이 존재하는지 확인
+            duplicate_count.put(duplicate_data.get(i), duplicate_count.get(duplicate_data.get(i))  + 1);  // key가 이미 있다면 value에 +1
+        } else { // key값이 존재하지 않으면
+            duplicate_count.put(duplicate_data.get(i) , 1); // key 값을 생성후 value를 1로 초기화
+        }
     }
 
-    public Parent(String nation) {
-        this.nation = nation;
-        System.out.println("Parent(String nation) call");
-    }
+    System.out.println(duplicate_count);
+}
 }
