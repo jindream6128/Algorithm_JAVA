bec1947c69041fe566990963263d879a3cbf77aa
jindream6128 <jindream6128@naver.com>
2023-04-01 17:15:10 +0900

    Algorithm 0401 bronze5

---
+++ BOJ/Bronze/boj_15727.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj_15727 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int l = Integer.parseInt(br.readLine());
+        int ans = l/5;
+        if(l%5 != 0){
+            ans ++;
+        }
+        System.out.println(ans);
+    }
+}
